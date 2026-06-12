16a7ffb835f0cd0c7c9d821dc79bfd08b5ce4a2c
Gabe <contatogabemelo@gmail.com>
2026-06-12 18:03:14 -0300

    feat(backend): initial backend setup

---
+++ backend/src/main/java/com/gabecmelo/cidade_agil/CidadeAgilApplication.java
@@ -0,0 +1,13 @@
+package com.gabecmelo.cidade_agil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidadeAgilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeAgilApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabecmelo/cidade_agil/CidadeAgilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabecmelo.cidade_agil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidadeAgilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
